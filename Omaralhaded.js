500c04251c0d61ba5f99f8c2f08d6808ef1aad25
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2021-01-05 20:52:58 +0200

    Create Omaralhaded.js

---
+++ Omaralhaded.js
@@ -0,0 +1,83 @@
+if(location.protocol!='https:'){location.protocol='https:';} 
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://chatnsh.com/sico/z1eop1s0h810.jpg"> <a id="jfa44" target="_blank" type="a" rel="nofollow" href="https://www.alhaded.com" class="label-primary">Omar AL-Haded</a><center>
+
+<a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a>
+<a id="asim10" target="_blank" type="a" href="" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">اتصل بنا</a><a id="asim10" target="_blank" type="a" href="" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">التطبيق</a><a id="asim10" target="_blank" type="a" href="" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">الاشتراكات</a><a id="asim10" target="_blank" type="a" href="" style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">اختصارات</a><a id="asim10" target="_blank" type="a" href="" style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">القوانين</a>`).insertBefore('.nav-tabs');
+$(`<a href="" target="_blank" style=" font-family: \'El Messiri\', sans-serif;width:100px;color: #E8415D;text-decoration:none; margin-right: 3px;\x0D\x0Amargin-left: 2px;text-align: center;float: right;border-radius: 5px;\x0D\x0Aborder-color: #F2F2F2;\x0D\x0Apadding: 4.0px;" class="border" ><i class="fa fa-forumbee"></i>&nbsp;تطبيق الشات</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.border');
+
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+
+$('.checkbox label')['html'](`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://chatnsh.com/sico/z1er6hk7js10.gif" width="70%" style="">`);
+
+/*$('div#tlogins button.btn.btn-primary')['click'](function () {
+    var b = setInterval(function () {
+        var a = $('.pmsgc')['length'];
+        0 < a ? ($(`</div><center>\x0D\x0A</div><center><a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp; محرر ومعدل الصور بـ احترافيه للجميع</a>\x0D\x0A<iframe type="text/html" id="jfa200" width="85%" height="100%" src="https://www.photoeditoangel.com/photo-editor/pixie/" scrolling="no" frameborder="0"></iframe>\x0D\x0A<iframe type="text/html" id="jfa150" width="100%" height="100px" src="https://reach-audio.esteam.rocks/public/nogoum_fm/embed" scrolling="no" frameborder="0"></iframe>\x0D\x0A<img id="jfa180" border="0" src="https://mrkzgulfup.com/uploads/160655645727691.jpg" /></a>\x0D\x0A<a id="jfa160"target="_blank"type="a"href="https://t.me/ihtc0"style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">أتصل بنأء</a>\x0D\x0A<center>\x0D\x0A\x0D\x0A`)['appendTo']('#mic'), clearInterval(b)) : console['log'](a)}, 1)})
+*/
+$(`<marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="jfa99">♥ || احبــــك  || ♥</font> ● · &#8235;أفضل سوبره لهذا الأسبوع · ● <font class="jfa99" color="#727A54">&nbsp;قريباً ♥ &nbsp;</font> ● · &#8235;أفضل زائر لهذا الأسبوع · ● <font class="jfa88" color="#ff9900">&nbsp;قريباً &nbsp;</font> ● · &#8235;أفضل زائرة لهذا الأسبوع · ● <font class="jfa88" color="#ff00ff">&nbsp; قريباً .. ♥  &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="jfa88" color="#5CAD9A">&nbsp; قريباً  &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="jfa88" color="#800000">&nbsp; قريباً &nbsp;</font> ● · &#8235;أفضل أدارية لهذا الأسبوع · ● <font class="jfa88" color="#417570">&nbsp; قريباً .. ♥  &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp; 
+</marquee>`).insertBefore('div#tlogins .lonline.light.break');
+
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/m/e846a72b0d06206cdbd12aca8c976426cac7bce8/Omaralhaded.css"></style>`).insertBefore('body');
+
+$('.fl.ustat')['css']('width', '3px')
+
+$('.fa.fa-user-plus')['html'](`${'❥تسجيل '}`)
+document['getElementById']('u1')['placeholder'] = 'اكتب اسمك من 5 احرف فقط '
+document['getElementById']('pass1')['placeholder'] = 'كلمة المرور'
+document['getElementById']('u2')['placeholder'] = 'فخأمة طلتك تنورنا' 
+document['getElementById']('pass2')['placeholder'] = ' ㋡ بـاسوردك ولاتنساه يـ جميل '
+document['getElementById']('u3')['placeholder'] = '♥ أكتب اسمك ولازم تحفظه ';
+
+$('div#tlogins button.btn.btn-primary')['click'](function () {
+    var b = setInterval(function () {
+        var a = $('.pmsgc')['length'];
+        0 < a ? ($(`<a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp; لـ الشكاوي .والاقتراحات . </a><input type="jfa-w" name="FirstName" value="( @ihtc0 ) اتصال بناء: عبر تليجرام"><center>`)['appendTo']('#settings'), clearInterval(b)) : console['log'](a)}, 1)});
+        
+$('#d0 .fa-gear')['html'](' ')['css']('width', '36px');
+
+$('#d0 .fa-users')['html'](' ')['css']('width', '36px');
+
+$('label.ae.fa.label.label-primary.fa-commenting-o')['html']('<span class="bwall  minix badge border" style="margin-top:-8px;padding:1px 4px;">0</span>');
+ $('.u-msg.dots').removeClass('dots');  
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b>الاشتراكــات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> تاج ذهبي بخاصية ( المخفي ) + ( سوبر فضي هديه ) بـ 600 ريال لمدة : شهر <img src="sico/z1er7ta61310.gif"></p><div style="float: right;padding-right: 20px;">- 2</div> تاج اسود ملكي بخاصية ( المخفي ) بـ 500 ريال لمدة : شهر <img src="sico/z1er7ta98o10.gif"></p><div style="float: right;padding-right: 20px;">- 3</div> تاج فضي بـ 400 ريال لمدة : شهر <img src="sico/z1er7tacbf10.gif"> </p><div style="float: right;padding-right: 20px;">- 4</div> نجمه ذهبيه بـ 300 ريال لمدة : شهر <img src="sico/z1er7taf3510.gif"></p><div style="float: right;padding-right: 20px;">- 5</div> نجمه سوداء بـ 150 ريال لمدة : شهر <img src="sico/z1er7tahp710.gif"></p><div style="float: right;padding-right: 20px;">- 6</div> نجمه فضيه بـ 100 ريال لمدة : شهر <img src="sico/z1er7tak6e10.gif"><p><b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهر</font></b><p style="text-align: center"><u><b><a target="_blank" href="">وللتواصل مع الاداره داخل شات رانيا & عمر </a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+
+$(' div#l1 button.btn.btn-primary ,div#l2 button.btn.btn-primary , div#l3 button.btn.btn-primary').on("click", function () {
+    setTimeout(function () {
+console.log('minutes');
+alert('كم نحن سعداء ..عندما أبقتنا الأقدار إلى هذا اليوم ..هنا نحتفل وهنا.. نرسم عبارات افراحنا فأسعد الله اوقاتكم بكل ماهو جميل دخل شات احبك للجوال  .. عامين أعوام انقضت انقضت . بالحب .. بالصدق .. بالإخلاص .. بالوفاء ... انقضت بالحنان .. بالطيبه ... بالصداقه ... بالمعرفه ... عام ثالث ومولد جديد.... أيــادي متماسكه ،، وقلوب مترابطه زهو يشعل نور أحلامنا ويطفئ شموع عامه الماضي مستقبلاً عامه الجديد اقدم لكم اجمل تهنئة بدخول  شات احبك عامه الثاني و اتمنى له الاستمرار و النجاح و التقدم المستمر ');
+
+alert('فنحمد الله ونشكرة على توفيقة لنا خلال مسيرة هذا الشات الشامخ لعامين متتالين وأنا أهنى جميع اعضاء ورواد هذا الشات بهذة المناسبة والذكرى الجميلة وكلنا لاينسى بداية هذا الشات ولاينسى احبتنا من اعضائة الذين اسهمو في تطويره من اعضاء ومشرفين ومراقبين وادارهـ كل منهم سعى للنجاح واجتهد وضحى بوقتة وجهدة في سبيل رقي وعلو شات احبك واننى انتهز هذة الفرصة لأشكركم جميعا على ماقدمتموه فانتم الوقود الحقيقي لهذا الشات الذى جمعنا على الخير وعلى الحب والتقدير انقضى عامان ولاكن لازال للاجتهاد بقية وهذا سوف يتحقق بمشيئة الله تعالى وبفضلة ثم بفضلكم جميعا ');
+
+alert('وختاما اسأل الله ان يوفق الجميع ويسدد خطاهم اخوكم ومحبكم  عــمر ألحديــــد');
+
+
+}, 1000);
+}); 
+$("[data-target=#wall]").attr('title',' "  الحائط للابداع يمنع السوالف .. ')  	
+$(".ubnr").remove(); 
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
+bct=100;msgt=100;
+
+/*
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"><u><b>الاشتراكــات</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" ><p style="text-align: center"><b><font color="#000080">اشتراكات الشات</font></b></p></p><div style="float: right;padding-right: 20px;">- 1</div> تاج ذهبي بخاصية ( المخفي ) + ( سوبر فضي هديه ) بـ 600 ريال لمدة : شهر <img src="sico/z1er7ta61310.gif"></p><div style="float: right;padding-right: 20px;">- 2</div> تاج اسود ملكي بخاصية ( المخفي ) بـ 500 ريال لمدة : شهر <img src="sico/z1er7ta98o10.gif"></p><div style="float: right;padding-right: 20px;">- 3</div> تاج فضي بـ 400 ريال لمدة : شهر <img src="sico/z1er7tacbf10.gif"> </p><div style="float: right;padding-right: 20px;">- 4</div> نجمه ذهبيه بـ 300 ريال لمدة : شهر <img src="sico/z1er7taf3510.gif"></p><div style="float: right;padding-right: 20px;">- 5</div> نجمه سوداء بـ 150 ريال لمدة : شهر <img src="sico/z1er7tahp710.gif"></p><div style="float: right;padding-right: 20px;">- 6</div> نجمه فضيه بـ 100 ريال لمدة : شهر <img src="sico/z1er7tak6e10.gif"><p><b>ومن اراد بنر لـ اسمه بـ 100 ريال مع التصميم ولمدة شهر</font></b><p style="text-align: center"><u><b><a target="_blank" href="">وللتواصل مع الاداره داخل شات رانيا & عمر </a></b></u></font></div> </div></div><center>`).insertBefore('#d2');$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+if(location.protocol!='https:'){location.protocol='https:';} 
+$(`<img src="sico/z1er7u1m9a10.gif" class="fr" style="width:100%;margin-right: 2px;">
+<a id="des3" href="" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-graduation-cap btn btn-primary"> رمزيات</a>
+<a id="des3" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star btn btn-default" href="">زخارف</a>
+<a id="des3" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap btn btn-primary" href="">الإشتركات  
+</a>
+<a id="des3" style="border-radius: 24px;width:22%;margin:2px;" class="fl mini  fa fa-phone btn btn-default" href="">الادارة</a>
+
+<div style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">         اداره الموقع           </div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  &nbsp;<i class="fa fa-paper-plane">  &nbsp;</i> اداره الموقع </p>     <i class="fa fa-comment"></i> <i class="fa fa-share"> </i> </p>    <i class="fa fa-comment"></i> <i class="fa fa-share">    ‏ | شات احبك    </i> </p>  <i class="fa fa fa-comment"></i> <i class="fa fa-share">     </i>          </div></div><center></div></div><center><center><div><marquee direction="right" wi
+
+`).insertBefore('.nav-tabs');
+vchat=true;
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;اريحو : مسإمعكم ..</a><input type="jfa-w" name="FirstName" value="  اجمل شات احبك"><audio src="https://j.top4top.io/m_1592gehs81.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
+$("[data-target=#wall]").attr('title',' "  الحائط للابداع يمنع السوالف ..   ')
+$(`<marquee class="label-primary fl" direction="right" width="99%" style="padding-top:0px;font-family: 'jazeera-light',FontAwesome;height: 23px;color: white;border-radius: 2px;margin: 0 2px 2px!important;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"
+> . آفضل سوبر عام لهذا الاسبوع . <span style="background-color: white;border-radius: 10px;padding: 0px 7px 0;margin-top: 1px;border-top: 1px solid #fff0;color: #000"> قريبا </span> . آفضل سوبر مسابقات لهذا الاسبوع  .<span style="background-color: white;border-radius: 10px;padding: 0px 7px 0;margin-top: 1px;border-top: 1px solid #fff0;color: #000"> قريبا </span> . آفضل سوبر فعاليات لهذا الاسبوع  .<span style="background-color: white;border-radius: 10px;padding: 0px 7px 0;margin-top: 1px;border-top: 1px solid #fff0;color: #000"> قريبا ..ِ  </span> مع تمنياتنا للجميع بـ أطيب الاوقات  " آدارة " شآت احبك للجوال " 
+ </marquee>`).insertBefore('.lonline.light.break');
+ $('.u-msg.dots').removeClass('dots');  
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات احبك ")
+
+*/
